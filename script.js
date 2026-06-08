76a6b9665c4cf08b1602aa6946b2c4adc4df0a37
Priam98 <113326295+Priam98@users.noreply.github.com>
2026-06-08 17:33:50 +0700

    Implement button that evades mouse cursor
    Added a new button that moves away from the mouse cursor when it gets too close.

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ years.forEach(button => {
     });
 
 });
+
+const tombolKabur = 
+document.querySelector('.kabur');
+document.addEventListener('mousemove', (e) =>{
+    const react = tombolKabur.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const dx = e.clientX - centerX;
+    const dy = e.clientY - centerY;
+    const jarak = Math.sqrt(dx*dx + dy*dy);
+    if (jarak < 150) {tombolKabur.style.transform = `translate(${-dx*0.3}px, ${-dy*0.3}px)`;
+                     }
+});
+                          
