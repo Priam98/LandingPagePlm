567465e47e381cba4db8103ed0fe9ae354286966
Priam98 <113326295+Priam98@users.noreply.github.com>
2026-06-17 23:23:15 +0700

    Delete download_stockfish.py
    hapus file python, ga kepake juga

--- download_stockfish.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import os
-
-url = 'https://cdn.jsdelivr.net/gh/niklasf/stockfish.js/stockfish.js'
-out_path = os.path.join('Hiburan', 'stockfish.js')
-print('Downloading', url)
-urllib.request.urlretrieve(url, out_path)
-print('Saved', out_path)
